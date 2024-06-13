21c367df5d8022837717013d775acb8a434f96f8
isaactaveras <itaveras1989@gmail.com>
2024-06-13 21:15:00 +0200

    loading project

---
+++ MusicLibrary/src/test/java/com/ironhack/MusicLibrary/MusicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.MusicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
